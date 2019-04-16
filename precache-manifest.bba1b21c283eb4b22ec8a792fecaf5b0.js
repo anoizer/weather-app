f3a10d6081ab01301105807aee3e9fc2bb471970
Anoizer <anoizer@gmail.com>
2019-04-16 20:45:05 +0300

    Updates

---
+++ precache-manifest.bba1b21c283eb4b22ec8a792fecaf5b0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5b1d52d56a65924f526e",
+    "url": "/anoizer/static/css/main.4e5acd57.chunk.css"
+  },
+  {
+    "revision": "5b1d52d56a65924f526e",
+    "url": "/anoizer/static/js/main.ebd8473c.chunk.js"
+  },
+  {
+    "revision": "a4dd85bf925414800003",
+    "url": "/anoizer/static/js/runtime~main.8d045050.js"
+  },
+  {
+    "revision": "7f2cbbfedeb3203c0ab7",
+    "url": "/anoizer/static/css/2.f06731ab.chunk.css"
+  },
+  {
+    "revision": "7f2cbbfedeb3203c0ab7",
+    "url": "/anoizer/static/js/2.f3b9e38e.chunk.js"
+  },
+  {
+    "revision": "cc54c19b29890841267ee0750eb25a00",
+    "url": "/anoizer/static/media/polygon.cc54c19b.jpg"
+  },
+  {
+    "revision": "ddc9d64fb00f7a503b232c2a5a9af0d2",
+    "url": "/anoizer/index.html"
+  }
+];
